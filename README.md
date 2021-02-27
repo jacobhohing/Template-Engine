d2e4e3addbb85b4da58ca89713075d8447be50a5
Jacob Hohing <jakehohing@yahoo.com>
2021-02-27 10:00:20 -0500

    Updated README and added email validator

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Template-Engine-
+# Template-Engine
+
+This Project is designed to output an HTML displaying team members provided by the user.
+
+- Once the app is initiated, you will be prompted to provide detailed information for a team member (name, role, uniqueID, email, and a role specific input).
+- Once all members have been input, and the user selects the option not to continue adding members, a file will be generated.
+- The file can be found in the 'output' folder, labeled team.html. It will display all the team memebrs and their detail data in a structured format. 
+
+
+## Video Walkthrough of Prompts and File Generation
+![](assets/readMe.gif)
+
+## The Generated README
+![Project Img](assets/readMe.PNG)
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const Manager = require("./lib/Manager");
 
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const validator = require("email-validator");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -36,7 +37,13 @@ const questions = [
     {
         type: "input",
         message: "What is your email?",
-        name: "email"
+        name: "email",
+        validate(value) {
+            if (validator.validate(value)) {
+             return true;
+            }
+            return 'That was not a valid email address! Please try again.';
+        }
     },   
     {
         type: "input",
@@ -51,7 +58,7 @@ const questions = [
     {
         type: "input",
         message: "What is your Github account?",
-        name: "githun",
+        name: "github",
         when : (answers) => {
             if (answers.role === "Engineer") {
                 return true;
@@ -90,30 +97,26 @@ function init()
                 {
                     for(i = 0; i < tempEmployees.length; i++){
                         
-                        if(tempEmployees[i].role = "Manager")
+                        if(tempEmployees[i].role == "Manager")
                         {
                             const newEmp = new Manager(tempEmployees[i].name, tempEmployees[i].theID, tempEmployees[i].email, tempEmployees[i].officeNumber) 
                             employees.push(newEmp)                       
                         }
-                        else if (tempEmployees[i].role = "Engineer")
+                        else if (tempEmployees[i].role == "Engineer")
                         {
                             const newEmp = new Engineer(tempEmployees[i].name, tempEmployees[i].theID, tempEmployees[i].email, tempEmployees[i].github) 
                             employees.push(newEmp)
                         }
-                        else if (tempEmployees[i].role = "Intern")
+                        else if (tempEmployees[i].role == "Intern")
                         {
-                            const newEmp = new Manager(tempEmployees[i].name, tempEmployees[i].theID, tempEmployees[i].email, tempEmployees[i].school) 
+                            const newEmp = new Intern(tempEmployees[i].name, tempEmployees[i].theID, tempEmployees[i].email, tempEmployees[i].school) 
                             employees.push(newEmp)
                         }
 
-                       
-                        
-                        
 
                     }
-
                     const newRender = render(employees);
-                    console.log(newRender)
+                    writeHTML(outputPath, newRender)
                  
                 }
             })
@@ -122,25 +125,8 @@ function init()
 
 init();
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+function writeHTML(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+    err ? console.log(err) : console.log("The File was written successfully, your team is ready to view!");
+    });
+}
